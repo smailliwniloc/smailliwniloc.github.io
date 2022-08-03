c7352ea6b1d7ec4a46e35ebbb4abeab41357bc4b
smailliwniloc <smailliwniloc@gmail.com>
2022-08-03 15:00:15 -0500

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 import Games from './pages/games';
