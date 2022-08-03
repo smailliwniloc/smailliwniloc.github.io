d7336ab799657b623e8f02a780e6e048096ae1ee
smailliwniloc <smailliwniloc@gmail.com>
2022-08-03 14:02:05 -0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from "react";
-import Board from "./Board";
-import "./App.css";
 
-/** Simple app that just shows the LightsOut game. */
-
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Board nrows={5} ncols={5}/>
-      </div>
-    );
-  }
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Games from './pages/games';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+  
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={Home()} />
+        <Route path='/about' element={About()} />
+        <Route path='/games' element={Games()} />
+        <Route path='/annual' element={AnnualReport()} />
+        <Route path='/team' element={Teams()} />
+        <Route path='/blogs' element={Blogs()} />
+        <Route path='/sign-up' element={SignUp()} />
+      </Routes>
+    </Router>
+  );
 }
-
+  
 export default App;
