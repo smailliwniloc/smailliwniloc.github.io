31e79872acdc968d6a16b4313f6d4f1f6a7c23ed
smailliwniloc <smailliwniloc@gmail.com>
2022-08-09 15:15:28 -0500

    fixed link

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,7 +3,7 @@ import React from 'react';
   
 const Teams = () => {
   return (
-    <h1>No team just yet, but here is a <a href="/about">link</a> to the About page...</h1>
+    <h1>No team just yet, but here is a <a href="/#/about">link</a> to the About page...</h1>
   );
 };
   
